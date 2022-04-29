bd0f7d90dfd01eea93f755c26d40510282eb38f7
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-04-29 20:15:24 +0530

    Add project files.

---
+++ string revers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_revers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int length = str.Length - 1;
+
+            for (int i = length; i >=0; i--)
+            {
+                Console.Write(str[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
